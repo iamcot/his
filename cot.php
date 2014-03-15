4c4497f3e2bd251b1d76e917ac573ab5c34b4a60
123456 <h.nguyen.ptit@gmail.com>
2014-03-15 15:15:45 +0700

    update 2

--- cot.php
+++ cot.php
@@ -1,12 +1,14 @@
 <?php
 class test{
     public function cot(){
-
     }
     public function group(){
         //line cua cot
     }
     public  function tien(){
         //aaa
+    }
+    public function Hien(){
+
     }
 }
