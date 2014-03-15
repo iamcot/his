768f0edf9be13f12746148ca1c3ed435c9404fef
NgocMen <ngocmennguyen.gl@gmail.com>
2014-03-15 15:22:09 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	cot.php

--- cot.php
+++ cot.php
@@ -1,11 +1,13 @@
 <?php
 class test{
     public function cot(){
+
     }
     public function group(){
         //line cua cot
     }
     public  function tien(){
+        // hien khung oi oi oi
         //aaa
         //hienkhung
         // bbbbb
