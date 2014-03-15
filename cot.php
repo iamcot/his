5d466c85b4935cbe8743f8a533df456336a7ca04
123456 <h.nguyen.ptit@gmail.com>
2014-03-15 15:50:17 +0700

    update 5

--- cot.php
+++ cot.php
@@ -5,22 +5,6 @@ class test{
     }
     public function group(){
     }
-    public  function tien(){
 
-      
-    }
-    public function Hien(){
-    //abc
-      // abc xyzzzzzzzzzzzzzzzzzzz
-
-
-    }
-    public function phuongkhungabc() {
-       //
-    }
-    private  function  NgocMen()
-    {
-        //HÉ LÔ MẤY BẤY BI
-    }
 
 }
