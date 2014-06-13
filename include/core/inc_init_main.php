9ac2df0e3976bfca2d2242d7f9b6233781841af5
CamNang269 <camnang269@gmail.com>
2014-06-13 08:42:29 +0700

    db

--- include/core/inc_init_main.php
+++ include/core/inc_init_main.php
@@ -1,7 +1,7 @@
 <?php
 # This is the database name
 #$dbname='histudb';
-$dbname='his';
+$dbname='histudb';
 
 # Database user name, default is root or httpd for mysql, or postgres for postgresql
 $dbusername='root';
