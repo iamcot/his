59e793a4a6df5e7766cb61b04f7f77e1bcaad5c9
MINHPHUONG <phamminhphuongstudent@gmail.com>
2014-06-14 14:44:57 +0700

    update tên

--- modules/nursing/nursing-issuepaper-depot.php
+++ modules/nursing/nursing-issuepaper-depot.php
@@ -342,7 +342,7 @@ $smarty->assign('LDTT',$LDTamThan);  //đã thêm
 if($target=='depot') 
 {
 	$smarty->assign('depot',true);
-	$smarty->assign('sTypePut','<select name="typeput" class="input1" onChange="alertselected(this)"><option value="0">'.$LDBH.'</option><option value="1" selected>'.$LDNoBH.'</option><option value="2">'.$LDCBTC.'</option></select>');
+	$smarty->assign('sTypePut','<select name="typeput" class="input1" onChange="alertselected(this)"><option value="0">'.$LDBH.'</option><option value="1">'.$LDNoBH.'</option><option value="2">'.$LDCBTC.'</option></select>');
 
 	$smarty->assign('AddRow','<a href="javascript:;" onclick="insertRow();">&nbsp;[+]&nbsp;'.$LDAddRowMedicine.'</a>');
 	$type=0; $style=''; $readonly='';
