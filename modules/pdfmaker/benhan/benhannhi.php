bd0e5417b027530aa5849fd0d745a5240f0d933c
tmchiennguyen <h.nguyen.ptit@gmail.com>
2014-06-14 15:34:29 +0700

    test

--- modules/pdfmaker/benhan/benhannhi.php
+++ modules/pdfmaker/benhan/benhannhi.php
@@ -217,7 +217,7 @@ $tb1='
 			<td>1. Họ và tên <i>(In hoa)</i>   '.str_pad("..".$s,30, ".", STR_PAD_RIGHT).'</td>
 			
 			
-			<td align="left">2. Sinh ngày:';
+			<td align="left">2. Sinh ngày:';
 if($encounter['tuoi']<10){
     if(strlen($encounter['date_birth'])>4){
         $tb1.='&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.substr(formatDate2Local($encounter['date_birth'],$date_format),0,1)."&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),1,1)."&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),3,1)."&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),4,1)."&nbsp;&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),6,1)."&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),7,1)."&nbsp;&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),8,1)."&nbsp;&nbsp;&nbsp;".substr(formatDate2Local($encounter['date_birth'],$date_format),9,1)."&nbsp;&nbsp;&nbsp;".$tuoi;
