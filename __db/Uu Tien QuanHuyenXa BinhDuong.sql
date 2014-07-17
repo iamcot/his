bc96a606d76b6bc93712556278c031b887f7245c
123456 <h.nguyen.ptit@gmail.com>
2014-07-17 16:03:11 +0700

    Update changelog

--- __db/Uu Tien QuanHuyenXa BinhDuong.sql
+++ __db/Uu Tien QuanHuyenXa BinhDuong.sql
@@ -1,3 +1,5 @@
 UPDATE care_address_citytown SET use_frequency='99' WHERE nr=74
 UPDATE care_address_quanhuyen SET use_frequency='99' WHERE nr='723'
 UPDATE care_address_phuongxa SET use_frequency='99' WHERE nr='25888'
+UPDATE care_address_quanhuyen SET NAME='TX TÃ¢n UyÃªn' WHERE nr='723'
+
