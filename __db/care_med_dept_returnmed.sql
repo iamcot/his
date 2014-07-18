c649101ba7a9ef565163f35c42d2a7066ca86976
phuongnguyenhong <phuongnguyenhong275@gmail.com>
2014-07-18 09:53:13 +0700

    update CSDL

--- __db/care_med_dept_returnmed.sql
+++ __db/care_med_dept_returnmed.sql
@@ -4,4 +4,3 @@ ALTER TABLE `care_pharma_dept_returnmed` ADD `available_product_id` BIGINT(99)  
 
 ALTER TABLE `care_med_dept_destroymed` ADD `available_product_id` BIGINT(99)  NOT NULL  AFTER `note`;
 ALTER TABLE `care_pharma_dept_destroymed` ADD `available_product_id` BIGINT(99)  NOT NULL  AFTER `note`;
-
